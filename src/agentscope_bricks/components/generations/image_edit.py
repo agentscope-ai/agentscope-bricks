a19cc30b52f72119ab6205006df1ebc2fd114525
linkesheng.lks <linkesheng.lks@alibaba-inc.com>
2025-11-11 14:17:08 +0800

    feat(mcp): add description for watermark

--- src/agentscope_bricks/components/generations/image_edit.py
+++ src/agentscope_bricks/components/generations/image_edit.py
@@ -54,7 +54,7 @@ class ImageGenInput(BaseModel):
     )
     watermark: Optional[bool] = Field(
         default=None,
-        description="是否添加水印，默认不设置",
+        description="是否添加水印，默认不设置。可设置为true或false。",
     )
     ctx: Optional[Context] = Field(
         default=None,
